eddb600e5b152fe707169d63a6e407afded9a677
SarriaXD <sarria.qi.wang@gmail.com>
2024-06-30 02:30:45 -0500

    use video transform interface.

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-@app.route('/video_transform')
+@app.route('/video_transform', methods=['GET'])
 def video_transform():
-    return 'video_transform'
+    original_video_path = request.args.get('originalVideoPath')
+    return f'The original video path is: {original_video_path}'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=1234)
