5489adb8503e9b6a1dc7315ea8b4f49290be0ab0
SarriaXD <sarria.qi.wang@gmail.com>
2024-06-29 22:40:22 -0500

    add video transform interface

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/video_transform')
+def video_transform():
+    return 'video_transform'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
